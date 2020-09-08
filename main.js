218cdb987f74603ae95eb2467a738e2781e38db4
iskr889 <68342806+iskr889@users.noreply.github.com>
2020-09-08 12:51:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function mensaje() {
+
+    var nombre = document.getElementById("nombre").value;
+    var apellidos = document.getElementById("apellidos").value;
+    var mail = document.getElementById ("mail").value;
+
+    alert ('Buenardo Bro, ya nos comunicaremos con vos');  
+}
